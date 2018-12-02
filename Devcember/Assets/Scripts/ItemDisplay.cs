fa1004c3ce70351ce88eb6e1644680f87df62e5a
daviogg <daviogg0@gmail.com>
2018-12-02 20:53:02 +0100

    2# Devcember: created scriptable object

---
+++ Devcember/Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplay : MonoBehaviour {
+
+    public Item item;
+
+    public Text nameText;
+    public Image artwork;
+
+
+
+	void Start () {
+        nameText.text = item.name;
+        artwork.sprite = item.artwork;
+	}
+	
+}
